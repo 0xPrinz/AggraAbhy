e63530084febca0e94c1e9661a4241ca51bdfda1
0xPrinz <cryptoprinz69@gmail.com>
2024-09-09 23:41:48 +0800

    upload all files

---
+++ src/App.vue
@@ -0,0 +1,166 @@
+<template>
+  <!-- component -->
+  <!-- This is an example component -->
+  <div class="max-w-7xl mx-auto flex flex-col relative">
+
+    <nav class="max-w-7xl px-5 top-0 z-[98] w-screen backdrop-blur-md bg-[#121212] bg-opacity-80">
+      <div class="container mx-auto flex flex-wrap items-center justify-between">
+        <button @click="redirectToHome" class="flex">
+          <span class="self-center text-2xl text-[#00C9FF] font-semibold whitespace-nowrap fadein-bot hover:text-[#92FE9D]">0xPrinz</span>
+        </button>
+
+        <div class="flex md:order-2 fadein-bot">
+          <a href="https://www.tiktok.com/@0xprinz">
+            <img class="w-9 rounded-full" src="https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjk4Mi1kNS0wOC5wbmc.png" />
+          </a>
+        </div>
+        <div class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-3">
+          <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+            <li>
+              <router-link to="/"
+                class="fadein-bot text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0"
+                aria-current="page">Home</router-link>
+            </li>
+            <li>
+              <router-link to="/about"
+                class="fadein-bot fadein-1 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">About Me</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="md:mt-[100px]">
+      <router-view />
+    </div>
+  </div>
+  <footer class="fixed bottom-0 left-0 right-0 rounded-t-3xl border border-[#383838] bg-[#121212] bg-opacity-80 backdrop-blur-md backdrop-opacity-90 z-50 md:hidden">
+  <nav class="flex justify-around py-2 text-xs">
+    <router-link to="/" class="text-gray-300 hover:text-white">Home</router-link>
+    <router-link to="/about" class="text-gray-300 hover:text-white">About Me</router-link>
+  </nav>
+</footer>
+</template>
+
+<script>
+export default {
+  methods: {
+    redirectToHome() {
+      this.$router.push('/')
+    }
+  },
+}
+</script>
+
+
+<style>
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+::-webkit-scrollbar {
+  width: 5px; /* for vertical scrollbar */
+  height: 5px; /* for horizontal scrollbar */
+}
+
+::-webkit-scrollbar-track {
+  background: hsl(240, 1%, 17%);
+  border-radius: 5px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #92FE9D;
+  border-radius: 5px;
+}
+
+body {
+  font-family: 'Poppins', sans-serif;
+  background: hsl(0, 0%, 7%);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100vh;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  transition: color 0.3s;
+}
+
+nav a.router-link-exact-active {
+  display: inline-flex;
+  flex-direction: column;
+  color: white;
+  transition: color 0.3s;
+}
+
+nav a.router-link-exact-active::after {
+  display: inline-block;
+  content: "";
+  margin-top: 0.08em;
+  width: 100%;
+  height: 4px;
+  border-radius: 2px;
+  background-color: #92FE9D;
+}
+
+nav a.router-link-exact-active:hover {
+  color: white;
+}
+
+@media (max-width: 768px) {
+  footer nav {
+    flex-direction: row;
+    text-align: center;
+  }
+  
+  footer {
+    padding: 5px 0; /* Smaller padding */
+  }
+
+  footer nav a {
+    font-size: 10px; /* Smaller font size */
+    padding: 5px; /* Smaller padding for links */
+  }
+}
+
+@keyframes fadeInLeft {
+  0% {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+@keyframes shadowGradient {
+  0% {
+    box-shadow: 0px 0px 10px 2px rgba(0, 201, 255, 0.8);
+  }
+  100% {
+    box-shadow: 0px 0px 20px 5px rgba(146, 254, 157, 0.8);
+  }
+}
+
+img.w-9:hover {
+  animation: shadowGradient 1s linear infinite alternate;
+  transition: box-shadow 0.3s ease;
+}
+
+img.w-9 {
+  transition: box-shadow 0.3s ease; /* ensures smooth transition when not hovered */
+}
+
+</style>
